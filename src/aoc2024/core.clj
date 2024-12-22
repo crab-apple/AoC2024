9b488da1d9f467679175f080315abc22b7317612
crab-apple <andreu.bastardas@gmail.com>
2024-12-22 17:05:43 +0100

    Print solution to day 1-1

--- src/aoc2024/core.clj
+++ src/aoc2024/core.clj
@@ -1,4 +1,7 @@
-(ns aoc2024.core)
+(ns aoc2024.core
+
+  (:use
+   [aoc2024.day1.solution :as d1]))
 
 (defn -main []
-  (println (str "Hello, World!")))
+  (println "Day 01-1: " (d1/solution-1 (slurp "resources/day1/input"))))

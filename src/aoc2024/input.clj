294591ceac42ac707b41b4f3072a87a0ff390fa4
crab-apple <andreu.bastardas@gmail.com>
2024-12-22 14:27:21 +0100

    Fix definition order

--- src/aoc2024/input.clj
+++ src/aoc2024/input.clj
@@ -4,14 +4,14 @@
 (defn lines [in]
   (mapv str/trim (str/split-lines (str/trim in))))
 
+(defn numstr? [in]
+  (not (nil? (re-matches #"\d+" in))))
+
 (defn token [in]
   (if (numstr? in) (parse-long in) in))
 
 (defn tokens [in]
   (mapv token (str/split (str/trim in) #"\s+")))
-
-(defn numstr? [in]
-  (not (nil? (re-matches #"\d+" in))))
 
 (defn parse-input [in]
   (mapv tokens (lines in)))

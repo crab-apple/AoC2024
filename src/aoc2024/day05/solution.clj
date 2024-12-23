e5f11f1702b821d0dc6494a5484a74a16f9a0252
crab-apple <andreu.bastardas@gmail.com>
2024-12-24 00:09:13 +0100

    Day 5-1

---
+++ src/aoc2024/day05/solution.clj
@@ -0,0 +1,47 @@
+(ns aoc2024.day05.solution
+  (:require [aoc2024.input :as in]
+            [clojure.string :as str]))
+
+(defn parse-input [input]
+  (let [lines (in/lines input)
+        rule-lines (filter #(str/index-of %1 "|") lines)
+        update-lines (filter #(str/index-of %1 ",") lines)]
+    {:rules   (map #(str/split %1 #"\|") rule-lines)
+     :updates (map #(str/split %1 #",") update-lines)}))
+
+(defn- page-set [update]
+  (set update))
+
+(defn contains-page [update page]
+  (contains? (page-set update) page))
+
+(defn rule-applies [update rule]
+  (and
+    (contains-page update (first rule))
+    (contains-page update (second rule))))
+
+(defn- appliable-rules [update candidate-rules]
+  (set (filter (partial rule-applies update) candidate-rules)))
+
+(defn valid-update? [rules update]
+  (let [rules (appliable-rules update rules)]
+    (case (count update)
+      0 true
+      1 true
+      (let [head (first update)
+            violated-rules (set (filter (fn [rule] (= (second rule) head)) rules))
+            satisfied-rules (set (filter (fn [rule] (= (first rule) head)) rules))]
+        (if (not-empty violated-rules)
+          false
+          (valid-update? (apply disj rules satisfied-rules) (rest update)))))))
+
+(defn- mid-value [seq]
+  (let [mid (/ (dec (count seq)) 2)]
+    (nth seq mid)))
+
+(defn solution-1 [input]
+  (let [rules (:rules (parse-input input))
+        updates (:updates (parse-input input))
+        valid-updates (filter (partial valid-update? rules) updates)
+        ]
+    (apply + (map parse-long (map mid-value valid-updates)))))
---
+++ test/aoc2024/day05/solution_test.clj
@@ -0,0 +1,93 @@
+(ns aoc2024.day05.solution_test
+  (:use [aoc2024.day05.solution]
+        [midje.sweet]))
+
+(facts "valid-update?"
+       (fact "an empty update is always valid"
+             (valid-update? [[1 2] [3 4]] []) => true)
+       (fact "an update with only one page is always valid"
+             (valid-update? [[1 2] [3 4]] [2]) => true)
+       (fact "an update with no rules is always valid"
+             (valid-update? [] [1 2 3 4]) => true)
+       (fact "an update with rules that don't match in any number is always valid"
+             (valid-update? [[1 2] [3 4]] [5 6 7 8]) => true)
+       (fact "an update with rules that match in only one number is always valid"
+             (valid-update? [[1 2] [3 4]] [1 4 7 8]) => true)
+       (fact "an update with numbers in opposite order as a rule is invalid"
+             (valid-update? [[1 2]] [2 1]) => false
+             (valid-update? [[1 2]] [3 2 1]) => false
+             (valid-update? [[1 2]] [2 3 1]) => false))
+
+(facts "update"
+       (let [update [1 2 3 8 7 6]]
+         (facts "contains-page"
+                (fact "is true for contained pages"
+                      (contains-page update 1) => true
+                      (contains-page update 3) => true
+                      (contains-page update 8) => true)
+                (fact "is false for not contained pages"
+                      (contains-page update 4) => false
+                      (contains-page update 0) => false))
+         (facts "rule-applies"
+                (fact "rule applies if the update contains both sides of the rule"
+                      (rule-applies update [2 3]) => true
+                      (rule-applies update [2 7]) => true
+                      (rule-applies update [7 2]) => true)
+                (fact "rule does not apply if the update contains only one side of the rule"
+                      (rule-applies update [2 9]) => false
+                      (rule-applies update [9 2]) => false)
+                (fact "rule does not apply if the update contains none of the sides of the rule"
+                      (rule-applies update [0 9]) => false
+                      (rule-applies update [9 0]) => false))))
+
+(facts "parse-input"
+       (let [input
+             "
+             75|13
+             53|13
+
+             75,47,61
+             97,61,53
+             "]
+         (fact "parses the correct number of rules"
+               (count (:rules (parse-input input))) => 2)
+         (fact "parses the correct rules"
+               (:rules (parse-input input)) => [["75" "13"] ["53" "13"]])
+         (fact "parses the correct number of updates"
+               (count (:updates (parse-input input))) => 2)
+         (fact "parses the correct updates"
+               (:updates (parse-input input)) => [["75" "47" "61"] ["97" "61" "53"]])))
+
+(facts "solution-1"
+       (fact "works for example input"
+             (let [input "
+                   47|53
+                   97|13
+                   97|61
+                   97|47
+                   75|29
+                   61|13
+                   75|53
+                   29|13
+                   97|29
+                   53|29
+                   61|53
+                   97|53
+                   61|29
+                   47|13
+                   75|47
+                   97|75
+                   47|61
+                   75|61
+                   47|29
+                   75|13
+                   53|13
+
+                   75,47,61,53,29
+                   97,61,53,29,13
+                   75,29,13
+                   75,97,47,61,53
+                   61,13,29
+                   97,13,75,29,47
+                   "]
+               (solution-1 input) => 143)))
